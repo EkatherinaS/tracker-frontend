581a28ad7d12d67eed5ee89dc7bd6b6bd48321aa
Semchuk Ekaterina Alekseevna <e.semchuk@element-it.ru>
2023-09-05 17:29:53 +0500

    changed links

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React, { useState, ChangeEvent, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios'
+import * as Mui from '@mui/material';
+import * as MuiIcons from '@mui/icons-material';
 
 
 export default function App() {
 
   // переменные и состояния
-  const [inputTextName, setInputTextName] = useState("");
-  const [inputTextDescription, setInputTextDiscription] = useState('');
-  const [Start, setStart] = useState({
-    mode: false,
-    text: "start"
-  });
-
-
+  const [workType, setTypeOfWork] = React.useState('');
+  const [project, setProject] = React.useState('');
+  const [Start, setStart] = useState({ text: "start" });
   const [nowTime, setNowTime] = useState<Date>(new Date());
-  const getPadTime = (time: any) => time.toString().padStart(2, "0")
   const [timeleft, setTimeLeft] = useState(0)
+
+  const isDisabled = React.useMemo(() => !workType || !project, [workType, project])
+
+
+  const getPadTime = (time: any) => time.toString().padStart(2, "0")
 
   const hours = getPadTime(Math.floor(timeleft / 3600))
   const minutes = getPadTime(Math.floor((timeleft - hours * 3600) / 60));
@@ -24,34 +25,28 @@ export default function App() {
 
 
   const [isCounting, setIsCounting] = useState(false)
-  const [pause, setPause] = useState({
-    mode: true,
-    pause: false,
-    text: "Pause"
-  })
-  //const telegramData = (window as any).Telegram.WebApp
-  //const chat_Id = telegramData.initDataUnsafe.user.id;
+  const telegramData = (window as any).Telegram.WebApp
+  const chat_Id = telegramData.initDataUnsafe.user.id;
 
   const userData = {
-    Chat_Id: 1370990929,  // сделать аунтификацию в телеграмм
-    Name: inputTextName,
-    Description: inputTextDescription,
+    ChatId: chat_Id,  // сделать аунтификацию в телеграмм
+    Name: project,
+    Description: workType,
     StartTime: nowTime
   }
 
 
   const dateData = {
-    Chat_Id: 1370990929,
-    Hours: hours,
-    Minutes: minutes,
-    Seconds: seconds,
-    Name: inputTextName,
-    Description: inputTextDescription,
-    StartTime: nowTime
-
+    ChatId: chat_Id,
+    Name: project,
+    Description: workType,
+    StartTime: nowTime,
+    EndTime: nowTime
   }
 
   useEffect(() => {
+    setTypeOfWork("Электромонтажные работы");
+    setProject("1");
     const interval = setInterval(() => {
       isCounting &&
         setTimeLeft((timeleft) => (timeleft + 1));
@@ -64,82 +59,66 @@ export default function App() {
   // обработчики
 
   const handleClick = () => {
-    if (Start.text == "start") {
+    if (Start.text === "start") {
 
       const now = new Date()
-      setStart({ text: "stop", mode: true });
+      setStart({ text: "stop" });
       setNowTime(now);
       userData.StartTime = now
-      axios.post("https://eb54-89-250-212-72.ngrok.io/Task/StartButtonPost", userData).then((response) => {
+      axios.post("http://mongodbforallbots.element-it.ru/Task/StartButtonPost", userData).then((response) => {
       });
       setIsCounting(true);
-      setPause({ mode: false, text: "Pause", pause: false });
     }
 
     else {
-      setStart({ text: "start", mode: false });
-      setPause({ mode: true, text: "Pause", pause: false });
+      setStart({ text: "start" });
       setIsCounting(false)
-      setInputTextName("")
-      setInputTextDiscription("")
+      setProject("")
+      setTypeOfWork("")
       setTimeLeft(0)
       dateData.StartTime = nowTime
-      axios.post("https://eb54-89-250-212-72.ngrok.io/Task/StopButtonPost", dateData).then((response) => {
+      axios.post("http://mongodbforallbots.element-it.ru/Task/StopButtonPost", dateData).then((response) => {
       });
     }
   };
-  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputTextName(e.target.value);
-  };
-  const handleChangeDiscription = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputTextDiscription(e.target.value);
-  };
 
-  const handlePause = () => {
-    if (pause.pause === false) {
-      setPause({ mode: false, text: "Peset", pause: true });
-      setIsCounting(false)
-    }
-    else {
-      setPause({ mode: false, text: "Pause", pause: false });
-      setIsCounting(true)
-    }
-
-  };
-
-  let mode = true;
-  if (inputTextName.length > 1 && inputTextDescription.length > 1) {
-    mode = false;
-  }
-  else {
-    mode = true;
-  }
 
 
   return (
-    <div>
-      <h2>Введите название задачи</h2>
-      <input onChange={handleChangeName} disabled={Start.mode} value={inputTextName}></input>
-      <h2>Введите описание задачи</h2>
-      <input onChange={handleChangeDiscription} disabled={Start.mode} value={inputTextDescription}></input>
-      {/* <div>{chat_Id}</div> */}
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h2>Выберите проект</h2>
+      <Mui.FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Mui.InputLabel id="choose-project">Проект</Mui.InputLabel>
+        <Mui.Select
+          labelId="choose-project-label"
+          id="choose-project"
+          value={project}
+          onChange={(event) => setProject(event.target.value)}
+          autoWidth
+          label="Project"
+        >
+          <Mui.MenuItem value={1}>Протон ПМ</Mui.MenuItem>
+        </Mui.Select>
+      </Mui.FormControl>
 
-      <div>
-        <div>Timer</div>
-        <div>{hours}:{minutes}:{seconds}</div>
-        <div>
-          {pause.mode ? (
-            <div>
-              <button onClick={handleClick} className='StartTimer' disabled={mode}>{Start.text}</button>
-            </div>
-          ) : (
-            <div>
-              <button onClick={handleClick} className='StartTimer' disabled={mode}>{Start.text}</button>
-              <button onClick={handlePause}>{pause.text}</button>
-            </div>
-          )}
-        </div>
-      </div>
+      <h2>Введите описание задачи</h2>
+      <Mui.TextField
+        id="typeOfWork"
+        label="Тип работы"
+        value={workType}
+        variant="standard"
+        onChange={(event) => setTypeOfWork(event.target.value)}
+      />
+
+      <h2>Таймер</h2>
+      <div>{hours}:{minutes}:{seconds}</div>
+
+      <Mui.IconButton onClick={handleClick} disabled={isDisabled} >
+        {Start.text === "start" ?
+          <MuiIcons.PlayArrowRounded sx={{ width: 200, height: 200, }} color={isDisabled ? 'inherit' : 'success'} /> :
+          <MuiIcons.StopCircle sx={{ width: 200, height: 200, }} color="error" />
+        }
+      </Mui.IconButton>
     </div>
   );
 }
